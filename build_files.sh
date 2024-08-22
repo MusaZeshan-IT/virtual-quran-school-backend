89e07401b113af21ca69e3f14f211a6cf705c20f
Musa Zeshan <musazeshan.it@gmail.com>
2024-08-23 01:44:35 +0500

    Resolved config settings again.

--- build_files.sh
+++ build_files.sh
@@ -4,10 +4,13 @@
 echo "BUILD START"
 
 # Install dependencies
-python3 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
-# Perform Django management tasks
-python3 manage.py migrate
+# Collect static files (optional, if you have any static files)
+python manage.py collectstatic --noinput --clear
+
+# Run migrations
+python manage.py migrate
 
 # Print end message
 echo "BUILD END"
