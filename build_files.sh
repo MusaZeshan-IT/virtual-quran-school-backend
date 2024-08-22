0787562c5ac868f9b42c7844c6001bd583ef1ac1
Musa Zeshan <musazeshan.it@gmail.com>
2024-08-23 01:31:10 +0500

    Resolved config settings again.

--- build_files.sh
+++ build_files.sh
@@ -4,10 +4,10 @@
 echo "BUILD START"
 
 # Install dependencies
-python3 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 # Run Django migrations and any other necessary commands
-python3 manage.py migrate
+python manage.py migrate
 
 # Print end message
 echo "BUILD END"
