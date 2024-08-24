3a935f147f30a9b5f08b75f0c34cade9b94652c7
Musa Zeshan <musazeshan.it@gmail.com>
2024-08-24 21:31:14 +0500

    Removed psycopg2 package and now only psycopg2-binary package is left which is required for deploying my backend to Vercel.

--- backend/settings.py
+++ backend/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "django-insecure-&+^#v*4(9cznr-%6kxi0dqa7y$prodldhv+szha+r3w4+^ni-r"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if ENVIRONMENT == "development":
