82e6f2274b57c012527f0cb3bcefe540b5ab2b00
Musa Zeshan <musazeshan.it@gmail.com>
2024-08-26 11:50:01 +0500

    Added whitnoise to installed apps and configured the urls file to handle media files.

--- backend/settings.py
+++ backend/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if ENVIRONMENT == "development":
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "courses",
     "payments",
     # Packages/Libraries
+    "whitenoise.runserver_nostatic",
     "rest_framework",
     "corsheaders",
     "rest_framework_simplejwt",
