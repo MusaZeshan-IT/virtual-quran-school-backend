5423f19aa0e2a51fa194bfd2a280bb17fc05c1c4
Musa Zeshan <musazeshan.it@gmail.com>
2024-09-02 20:50:55 +0500

    Added a new function to format the created at date.

--- blog/serializers.py
+++ blog/serializers.py
@@ -2,12 +2,14 @@
 
 from rest_framework import serializers
 from .models import Post
+from django.utils.dateformat import format
 
 
 class PostSerializer(serializers.ModelSerializer):
     """The serializer for the posts"""
 
     comment_count = serializers.SerializerMethodField()
+    created_at = serializers.SerializerMethodField()
 
     class Meta:
         """The meta data for the serializer"""
@@ -18,3 +20,7 @@ class PostSerializer(serializers.ModelSerializer):
     def get_comment_count(self, obj):
         """Function which counts comments"""
         return obj.comments.count()
+
+    def get_created_at(self, obj):
+        """Function which formats the created at date"""
+        return format(obj.created_at, "F j, Y")  # Format as "August 23, 2024"
