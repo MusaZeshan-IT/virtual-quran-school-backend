ae87839b947b8258782fcfa85a5d7d8e24d1c126
Musa Zeshan <musazeshan.it@gmail.com>
2024-09-12 12:00:43 +0500

    Added the the payments url in the main url's file.

--- payments/urls.py
+++ payments/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('payment/ipn/', views.payment_ipn, name='payment_ipn'),
+    path('ipn/', views.payment_ipn, name='payment_ipn'),
 ]
