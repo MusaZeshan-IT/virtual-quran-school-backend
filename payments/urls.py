351a7b1d4bf4df9f29ee922fc66c55613686f943
Musa Zeshan <musazeshan.it@gmail.com>
2024-09-13 19:01:11 +0500

    Added jazzcash infront of the initiate url to know that a jazzcash payment is being initiated.

--- payments/urls.py
+++ payments/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     path("ipn/", views.payment_ipn, name="payment_ipn"),
-    path("initiate/", views.initiate_payment, name="initiate_payment"),
+    path("jazzcash/initiate/", views.initiate_payment, name="initiate_payment"),
     path("callback/", views.payment_callback, name="payment_callback"),
 ]
