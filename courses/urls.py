44ca2de9e1da5a73cbe1652acd59d6bc1abcb289
Musa Zeshan <musazeshan.it@gmail.com>
2024-09-21 19:52:44 +0500

    Made the course plan name show on the checkout page.

--- courses/urls.py
+++ courses/urls.py
@@ -5,9 +5,7 @@ from . import views
 
 urlpatterns = [
     path("", views.CourseListView.as_view(), name="course-list"),
-    path(
-        "<slug:slug>/", views.CourseDetailView.as_view(), name="course-detail"
-    ),
+    path("<slug:slug>/", views.CourseDetailView.as_view(), name="course-detail"),
     path(
         "<slug:course_slug>/plans/",
         views.CoursePlanListView.as_view(),
